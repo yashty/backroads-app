55e9fbec8fe4b5a62637cf9b2263145215a0392a
yash <tyagiaysh2612@gmail.com>
2023-06-18 23:12:14 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import logo from "../images/logo.svg"
+import { pageLinks, socialLinks } from '../data';
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        {/* <!-- left this comment on purpose --> */}
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map((links) => {
+            return (
+              <li key={links.id}>
+                <a href={links.href}className="nav-link">
+                  {links.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <ul className="nav-icons">
+          {socialLinks.map((link) => {
+            const {id, href, icon} = link
+            return (
+              <li key={link.id}>
+                <a
+                  href={href}
+                  target="_blank"
+                  className="nav-icon"
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar
